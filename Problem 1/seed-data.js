6ddc575b7d1503dcd7daa10c6170947f3302bff9
Lili Boxer <ninaliboxer@gmail.com>
2019-10-29 17:52:07 -0700

    files set up for problem 1. dictinoary created

---
+++ Problem 1/seed-data.js
@@ -0,0 +1,19 @@
+const data = [
+  {
+  id: 0,
+  name: "John",
+  skills: ["javascript", "html", "css", "c#"]
+  },
+  {
+    id: 1,
+    name: "Brian",
+    skills: ["javascript", "java", "c", "c#", "c++", "html"]
+  },
+  {
+    id: 2,
+    name: "Michael",
+    skills: ["c", "c++", "go", "rust"]
+  }
+]
+
+module.exports = data;
--- Problem 1/skills.js
+++ Problem 1/skills.js
@@ -1,20 +1,4 @@
-const data = [
-  {
-  id: 0,
-  name: "John",
-  skills: ["javascript", "html", "css", "c#"]
-},
-{
-  id: 1,
-  name: "Brian",
-  skills: ["javascript", "java", "c", "c#", "c++", "html"]
-},
-{
-  id: 2,
-  name: "Michael",
-  skills: ["c", "c++", "go", "rust"]
-}
-]
+const data = require('./seed-data');
 
 function skills(data) {
 // take in data
